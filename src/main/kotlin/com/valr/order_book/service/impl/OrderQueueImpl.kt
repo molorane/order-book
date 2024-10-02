6e8b3133ccb0f97e480ae35696b5d87303810a45
mothusi <molorane91@gmail.com>
2024-10-02 22:32:54 +0200

    queue processing

--- src/main/kotlin/com/valr/order_book/service/impl/OrderQueueImpl.kt
+++ src/main/kotlin/com/valr/order_book/service/impl/OrderQueueImpl.kt
@@ -6,22 +6,18 @@ import com.valr.order_book.entity.enums.TakerSide
 import com.valr.order_book.exception.OrderProcessingException
 import com.valr.order_book.service.OrderQueue
 import org.springframework.stereotype.Component
-import java.util.*
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.PriorityBlockingQueue
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 import kotlin.random.Random
 
 @Component
 class OrderQueueImpl : OrderQueue {
 
-    val sellOrderComparator = Comparator<TradeOrder> { o1, o2 ->
-        o1.price.compareTo(o2.price) // Ascending order by price
-    }
+    val sellOrderComparator = compareBy<TradeOrder> { it.price } // Sell orders sorted by price ascending
 
-    val buyOrderComparator = Comparator<TradeOrder> { o1, o2 ->
-        o2.price.compareTo(o1.price) // Descending order by price
-    }
+    // Buy orders sorted by price (descending) and quantity (descending) if price is equal
+    val buyOrderComparator = compareByDescending<TradeOrder> { it.price }.thenByDescending { it.quantity }
 
     // Create a map to hold BlockingQueues
     val sellOrderMap: MutableMap<CurrencyPair, PriorityBlockingQueue<TradeOrder>> = mutableMapOf()
@@ -29,82 +25,87 @@ class OrderQueueImpl : OrderQueue {
     // Create a map to hold BlockingQueues
     val buyOrderMap: MutableMap<CurrencyPair, PriorityBlockingQueue<TradeOrder>> = mutableMapOf()
 
-    private val queue: BlockingQueue<TradeOrder> = LinkedBlockingQueue()
-
     // Create a map to hold live match invocations
-    val orderProcess: MutableMap<CurrencyPair, Int> = mutableMapOf()
+    val orderProcess: MutableMap<CurrencyPair, Boolean> = mutableMapOf()
+    private val lock = ReentrantLock()
 
     override fun addOrder(newOrder: TradeOrder) {
         if (newOrder.takerSide == TakerSide.SELL) {
             val sellOrders = sellOrderMap[newOrder.currencyPair]
             // If sellOrders is null, new sell currency pair should be created in a map
-            if (sellOrders == null) {
-                val newCurrencyPairQueue = PriorityBlockingQueue(10_000, sellOrderComparator)
-                newCurrencyPairQueue.put(newOrder)
-
-                synchronized(this) {
-                    if (sellOrderMap[newOrder.currencyPair] == null)
-                        sellOrderMap[newOrder.currencyPair] = newCurrencyPairQueue
-                    else
-                        sellOrderMap[newOrder.currencyPair]?.put(newOrder)
-                }
-
-            } else {
-                sellOrders.put(newOrder)
-            }
+            addOrder(sellOrderMap, newOrder, sellOrders, sellOrderComparator)
         } else {
             val buyOrders = buyOrderMap[newOrder.currencyPair]
             // If buyOrders is null, new buy currency pair should be created in a map
-            if (buyOrders == null) {
-                val newCurrencyPairQueue = PriorityBlockingQueue(10_000, buyOrderComparator)
-                newCurrencyPairQueue.put(newOrder)
-
-                synchronized(this) {
-                    if (buyOrderMap[newOrder.currencyPair] == null)
-                        buyOrderMap[newOrder.currencyPair] = newCurrencyPairQueue
-                    else
-                        buyOrderMap[newOrder.currencyPair]?.put(newOrder)
-                }
-
-            } else {
-                buyOrders.put(newOrder)
-            }
+            addOrder(buyOrderMap, newOrder, buyOrders, buyOrderComparator)
         }
-        queue.put(newOrder)
+    }
+
+    // This method is to avoid code duplication for adding sell orders and buy orders into a map
+    private fun addOrder(
+        orderMap: MutableMap<CurrencyPair, PriorityBlockingQueue<TradeOrder>>,
+        newOrder: TradeOrder,
+        orders: PriorityBlockingQueue<TradeOrder>?,
+        comparator: Comparator<TradeOrder>
+    ) {
+        if (orders == null) {
+            val newCurrencyPairQueue = PriorityBlockingQueue(10_000, comparator)
+            newCurrencyPairQueue.put(newOrder)
+
+            synchronized(this) {
+                if (orderMap[newOrder.currencyPair] == null)
+                    orderMap[newOrder.currencyPair] = newCurrencyPairQueue
+                else
+                    orderMap[newOrder.currencyPair]?.put(newOrder)
+            }
+
+        } else {
+            orders.put(newOrder)
+        }
     }
 
     override fun matchOrders(currencyPair: CurrencyPair): Pair<TradeOrder, TradeOrder>? {
-        val count = orderProcess[currencyPair] ?: 0
-
         // This means another thread with the same currency pair is currently doing order matching
         // Therefore, current thread must wait
-        // If count is 0, current thread does not need to wait
-        if(count > 0) {
-            synchronized(this) {
-                return match(currencyPair, count)
+
+        // Use the lock to ensure that checking and updating the busy status is thread-safe
+        lock.withLock {
+            // Check if another thread is busy with the same currency pair
+            val isThreadBusy = orderProcess[currencyPair] ?: false
+
+            return if (isThreadBusy) {
+                // Another thread is busy, so we will return null and not perform matching
+                return null
+            } else {
+                // Set the current thread as busy for this currency pair
+                orderProcess[currencyPair] = true
+
+                try {
+                    // Perform the match operation
+                    match(currencyPair)
+                } finally {
+                    // Reset the state after matching is done
+                    orderProcess[currencyPair] = false
+                }
             }
-        } else {
-            return match(currencyPair, count)
         }
     }
 
-    private fun match(currencyPair: CurrencyPair, count: Int) : Pair<TradeOrder, TradeOrder> {
-        orderProcess[currencyPair] = count.plus(1)
-
+    // This method is to avoid code duplication for adding sell orders and buy orders into a map
+    private fun match(currencyPair: CurrencyPair): Pair<TradeOrder, TradeOrder> {
         val sellOrders: PriorityBlockingQueue<TradeOrder> = sellOrderMap[currencyPair]
-            ?: throw  OrderProcessingException("Currency $currencyPair does not have sell orders")
+            ?: throw OrderProcessingException("Currency $currencyPair does not have sell orders")
 
         val buyOrders = buyOrderMap[currencyPair]
-            ?: throw  OrderProcessingException("Currency $currencyPair does not have buy orders")
+            ?: throw OrderProcessingException("Currency $currencyPair does not have buy orders")
 
         for (sellOrder in sellOrders) {
             for (buyOrder in buyOrders) {
-                if(buyOrder.price >= sellOrder.price) {
+                if (buyOrder.price >= sellOrder.price) {
                     // Remove the matched orders from the queues
                     buyOrders.remove(buyOrder)
                     sellOrders.remove(sellOrder)
 
-                    orderProcess[currencyPair] = count.minus(1)
                     return sellOrder to buyOrder
                 }
             }
@@ -114,9 +115,10 @@ class OrderQueueImpl : OrderQueue {
     }
 
     /*
-        This is a pseudo-method, that each worker thread must call to determine what orders to process
+        This is a pseudo-method, that each worker thread must call to determine what currency pair
+        This currency pair will be used to as a key to find orders that will be then be matched together
         My understanding is that there are situations where certain orders may need to be prioritized
-        If there are more orders on BTC than any other crypto, the crypto exchange may need to prioritize those
+        If there are more orders on BTC than any other crypto, the crypto exchange may need to prioritize those orders
         E.g. During a bull run, certain digital currency orders may surge and prioritizing them imply more profits for the business from trade fees
         However, on this implementation, I randomly select currency pairs to ensure that each currency pair in the map has an equal chance of being selected
         Also, I ensure that a currency pair selected has both sell orders and buy orders
@@ -131,12 +133,14 @@ class OrderQueueImpl : OrderQueue {
         val matchingCurrencyPairs = sellOrderMap.keys.intersect(buyOrderMap.keys).toList()
 
         // Check if there are matching orders
-        if (matchingCurrencyPairs.isNotEmpty()) {
-            val matchPair = matchingCurrencyPairs[Random.nextInt(matchingCurrencyPairs.size)]
-            return matchPair
+        return if (matchingCurrencyPairs.isNotEmpty()) {
+            matchingCurrencyPairs[Random.nextInt(matchingCurrencyPairs.size)]
         } else {
-            // This means there are no matching orders
-            return null
+            // This means there are no matching orders for a currency pair
+            // This will happen if
+            // 1. We have sell orders and no buy orders or the vice versa
+            // 2. If we have no orders at all
+            null
         }
     }
 }
---
+++ src/main/kotlin/com/valr/order_book/service/impl/TradeWorkerServiceImpl.kt
@@ -0,0 +1,76 @@
+package com.valr.order_book.service.impl
+
+import com.valr.order_book.entity.Trade
+import com.valr.order_book.entity.TradeOrder
+import com.valr.order_book.entity.enums.Status
+import com.valr.order_book.repository.OrderRepository
+import com.valr.order_book.repository.TradeRepository
+import com.valr.order_book.service.OrderQueue
+import com.valr.order_book.service.TradeWorkerService
+import org.slf4j.LoggerFactory
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+@Service
+class TradeWorkerServiceImpl(
+    private val orderQueue: OrderQueue,
+    private val tradeRepository: TradeRepository,
+    private val orderRepository: OrderRepository
+) : TradeWorkerService {
+    private val logger = LoggerFactory.getLogger(TradeWorkerServiceImpl::class.java)
+
+    @Transactional
+    override fun executeTrade(sellOrder: TradeOrder, buyOrder: TradeOrder) {
+        logger.info("Matched Trade -> SELL Order: $sellOrder with BUY Order: $buyOrder")
+
+        val matchedPrice = sellOrder.price // lowest sell price
+        val matchedQuantity = sellOrder.quantity.min(buyOrder.quantity)
+
+        // create a new trade entry
+        val newTrade = Trade(
+            seller = sellOrder.copy(),
+            buyer = buyOrder.copy(),
+            quantity = matchedQuantity,
+            price = matchedPrice,
+            tradedAt = LocalDateTime.now()
+        )
+
+        // save a new trade in a DB
+        tradeRepository.save(newTrade)
+
+        // Determine if a sell order is FILLED
+        updateTrade(sellOrder, matchedQuantity)
+
+        // Determine if a buy order is FILLED
+        updateTrade(buyOrder, matchedQuantity)
+    }
+
+    private fun updateTrade(order: TradeOrder, matchedQuantity: BigDecimal) {
+
+        if (order.quantity > matchedQuantity) {
+            val newOrder = order.copy(
+                quantity = order.quantity.minus(matchedQuantity),
+                status = Status.PARTIALLY_FILLED,
+                matchedQuantity = matchedQuantity.plus(matchedQuantity)
+            )
+
+            // update order
+            orderRepository.save(newOrder)
+
+            // Since the order is not FILLED, take PARTIALLY FILLED order back to the queue for further processing until it is filled
+            // This order may likely be executed by a different worker thread
+            orderQueue.addOrder(newOrder)
+        } else {
+            val newOrder = order.copy(
+                quantity = order.quantity.minus(matchedQuantity),
+                status = Status.FILLED,
+                matchedQuantity = order.quantity
+            )
+
+            // update order
+            orderRepository.save(newOrder)
+        }
+    }
+}
